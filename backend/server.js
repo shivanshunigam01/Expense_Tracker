830dae5e122ba1633741a25e08134b5a067b5cd8
Shivanshu Nigam <shivanshunigam8@gmail.com>
2025-04-14 11:20:23 +0530

    Some code formations

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`[${new Date().toISOString()}] 🚀 Server running at http://localhost:${PORT}`);
+  console.log(`[${new Date().toISOString()}]  Server running at http://localhost:${PORT}`);
 });
